b38186592ce4f158bfe8e8387d1191c249f5d48f
Alfonso Sarmiento Ráez <asarmiento@alintia.com>
2021-05-14 11:34:39 +0200

    asfadfsf

---
+++ net/code/test/TimeClient.java
@@ -0,0 +1,19 @@
+package net.code.java;
+
+import java.time.*;
+import java.lang.*;
+import java.util.*;
+
+public interface TimeClient {
+    void setTime(int hour, int minute, int second);
+    void setDate(int day, int month, int year);
+    void setDateAndTime(int day,  int month,  int year,
+                        int hour, int minute, int second);
+    LocalDateTime getLocalDateTime();
+    
+    static void MethodInsideInterface()
+    {
+    	int a = 0;
+    	
+    }
+}
